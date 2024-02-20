3e43e4402e4e470ac22c558233b44dcbfbb8923f
0xAckerMan <joelkores1@gmail.com>
2024-02-20 21:31:14 +0300

    validation for struct models

--- internal/data/orders.go
+++ internal/data/orders.go
@@ -1,8 +1,7 @@
 package data
 
 import (
-	"time"
-
+	"github.com/0xAckerMan/Savanah/internal/validator"
 	"gorm.io/gorm"
 )
 
@@ -12,10 +11,16 @@ type Order struct {
 	ProductID   uint      `json:"product_id" gorm:"not null"`
 	Quantity    int       `json:"quantity" gorm:"not null"`
 	OrderStatus string    `json:"order_status" gorm:"default:'placed'"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
 	Version     int       `json:"version" gorm:"version; default:1"`
 
 	Customer *Customer `gorm:"foreignKey:CustomerID"`
 	Product  *Product  `gorm:"foreignKey:ProductID"`
+}
+
+func ValidateOrder(v *validator.Validator, order *Order) {
+	v.Check(order.Quantity > 0, "quantity", "must be greater than zero")
+	v.Check(order.OrderStatus == "placed" || order.OrderStatus == "purchased", "order_status", "must be either 'placed' or 'purchased'")
+	v.Check(order.Version > 0, "version", "must be greater than zero")
+	v.Check(order.CustomerID > 0, "customer_id", "must be a positive integer")
+	v.Check(order.ProductID > 0, "product_id", "must be a positive integer")
 }
--- internal/data/products.go
+++ internal/data/products.go
@@ -1,6 +1,9 @@
 package data
 
-import "gorm.io/gorm"
+import (
+	"github.com/0xAckerMan/Savanah/internal/validator"
+	"gorm.io/gorm"
+)
 
 type Product struct {
     gorm.Model
@@ -8,4 +11,13 @@ type Product struct {
     Description string `json:"description"`
     Price       int    `json:"price" gorm:"not null"`
     Version     int    `json:"version" gorm:"version; default:1"`
+}
+
+func ValidateProduct(v *validator.Validator, product *Product) {
+    v.Check(product.ProductName != "", "product_name", "must be provided")
+    v.Check(len(product.ProductName) <= 50, "product_name", "must not be more than 500 bytes")
+    v.Check(product.Description != "", "description", "must be provided")
+    v.Check(len(product.Description) <= 600, "description", "must not be more than 1000 bytes")
+    v.Check(product.Price > 0, "price", "must be greater than zero")
+    v.Check(product.Version > 0, "version", "must be greater than zero")
 }
