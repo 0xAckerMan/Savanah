7121764742f30085322c35acbebd926839d96d69
0xAckerMan <joelkores1@gmail.com>
2024-04-01 13:41:50 +0300

    feat: Configured cors for dev purposes

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -3,11 +3,22 @@ package main
 import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+    "github.com/go-chi/cors"
 )
 
 func (app *Application) routes() chi.Router {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+	r.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 	r.MethodNotAllowed(app.methodNotAllowedResponse)
 
 	r.Get("/", app.Home)
@@ -19,7 +30,7 @@ func (app *Application) routes() chi.Router {
 			r.Get("/customers/me", app.GetMe)
 			r.Get("/customers/", app.handle_getCustomers)
 
-			//logout/signout
+			// logout/signout
 			r.Get("/logout", app.SignOutCustomer)
 
 			r.Route("/orders", func(r chi.Router) {
@@ -39,18 +50,14 @@ func (app *Application) routes() chi.Router {
 			})
 		})
 
-
 		r.Get("/healthcheck", app.healthcheck)
 		r.Route("/products", func(r chi.Router) {
 			r.Get("/", app.handle_getProduct)
 			r.Get("/{id}", app.handle_getSingleProduct)
 		})
 
-
-		
-			r.Post("/register", app.SignUpCustomer)
-			r.Post("/login", app.SignInCustomer)
-
+		r.Post("/register", app.SignUpCustomer)
+		r.Post("/login", app.SignInCustomer)
 	})
 
 	return r
